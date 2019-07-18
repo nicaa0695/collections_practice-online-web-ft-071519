8acc51149bddda187b41fda30808377b4f7b6c2d
Veronica Lopez <nicaa0695@gmail.com>
2019-07-18 21:53:23 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  array.sort do |x,y|
+    y <=> x 
+  end
+end
+  
+def sort_array_char_count(array)
+  array.sort! do |x,y| 
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array 
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  array.each do |word|
+    word.split word[2] = "$"
+  end
+end
+
+def find_a(array)
+  new_array = []
+  array.select do |word|
+    word.start_with? "a"
+  end
+end 
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n 
+  end
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1 
+      string
+    else 
+      string << "s"
+    end
+  end
+end
+    
